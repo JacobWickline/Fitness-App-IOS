ccac466fa95d2fa9661f05fecc786de4cd6337e6
Jacob Wickline <47604254+JacobWickline@users.noreply.github.com>
2023-12-07 19:57:32 -0500

    start

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Jacob Wickline on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
